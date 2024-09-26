78a3c97f826ac67f42ab5ab87026e1251b4e3105
narendrabasam <inarendra913@gmail.com>
2024-09-26 14:43:02 +0530

    Commiting code

---
+++ New.java
@@ -0,0 +1,23 @@
+package Fresher;
+
+public class New {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//defining an 'age' variable  
+		int age = 10;
+		
+		////checking the age  
+		
+		if (age<9) {
+			
+			System.out.println("Age is lessthan 9");
+			
+		}
+		else {
+			System.out.println("false");
+		}
+	}
+
+}
